434c976727fe48a7c6c34f9febdcba575f3d51c0
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2018-05-02 17:31:38 +0200

    added inquiry and related classes

---
+++ src/Domain/TypeOfRelative.java
@@ -0,0 +1,24 @@
+package Domain;
+
+/**
+ * @author Mathias
+ */
+public enum TypeOfRelative {
+    //Consider changing this from hardcoded to something else.
+    FATHER  ("Far"),
+    MOTHER  ("Mor"),
+    DAUGHTER("Datter"),
+    SON     ("Søn"),
+    AUNT    ("Tante"),
+    UNCLE   ("Onkel");
+
+    private final String name;
+    TypeOfRelative(String name){
+        this.name=name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}

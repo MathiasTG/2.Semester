fb15a73d7ea984223c00c87897f97aebc8c2de8f
Oskar <skakkristiansen@gmail.com>
2018-05-03 13:31:32 +0200

    Created classes GatheredConsent & Municipality

--- src/Domain/Inquiry/GatheredConsent.java
+++ src/Domain/Inquiry/GatheredConsent.java
@@ -1,4 +1,30 @@
 package Domain.Inquiry;
 
 public class GatheredConsent {
+
+    private ConsentEntity consentEntity;
+    private String contactInfo;
+
+    public GatheredConsent(ConsentEntity consentEntity,
+                           String contactInfo){
+        this.consentEntity = consentEntity;
+        this.contactInfo = contactInfo;
+    }
+
+    public void setConsentEntity(ConsentEntity consentEntity) {
+        this.consentEntity = consentEntity;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public ConsentEntity getConsentEntity() {
+
+        return consentEntity;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
 }

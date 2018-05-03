1fa9120e2e7f14bb215ae2572dfa0bca2259ff00
Oskar <skakkristiansen@gmail.com>
2018-05-03 13:22:46 +0200

    Created classes ConsentEntity, ConsentType, Submitter, SubmitterType

--- src/Domain/Inquiry/ConsentEntity.java
+++ src/Domain/Inquiry/ConsentEntity.java
@@ -1,4 +1,24 @@
 package Domain.Inquiry;
 
 public enum ConsentEntity {
+
+    PERSONAL_DOCTOR         ("personlig læge"),
+    SPECIAL_DOCTOR          ("special læge"),
+    HOSPITAL                ("hospital"),
+    UNEMPLOYMENT_FUND       ("arbejdsløshedsfond"),
+    OFFER                   ("tilbud"),
+    PREVIOUS_MUNICIPALITY   ("tidligere kommune"),
+    OTHER_MANAGEMENT        ("anden ledelse"),
+    OTHER                   ("anden");
+
+
+    private final String name;
+    ConsentEntity(String name){
+        this.name=name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }

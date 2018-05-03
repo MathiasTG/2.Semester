c1d7044bf0c68612487dd655e68b61d95e408f2c
Casper Hansen <caspha17@gmail.com>
2018-05-03 14:08:09 +0200

    Added attributes to Inquiry

--- src/Domain/Inquiry/Inquiry.java
+++ src/Domain/Inquiry/Inquiry.java
@@ -2,17 +2,37 @@ package Domain.Inquiry;
 
 import Domain.User;
 
+import java.util.List;
+import java.util.UUID;
+
 /**
  * @author mathias
  */
 public class Inquiry {
     private Citizen citizen;
-    //private Details details;
-    /*
-      vi skal ha lavet en løsning til at gemme alle detaljerne i en henvendelse.
-      hvordan vil det bliver sendt fra UI laget.
-     */
     private boolean isDraft;
     private boolean supportsVUM;
     private User createdBy;
+
+
+    private UUID id;
+    private String desciption;
+    private boolean isIntentIsClear;
+    private boolean isCitizenInformedOfRights;
+    private boolean isCitizenInformedOfDataReservation;
+    private String agreementOfProgress;
+    private ConsentType consentType;
+    private List<GatheredConsent> gatheredConsents;
+    private String specialConditions;
+    private Municipality actingMunicipality;
+    private Municipality payingMunicipality;
+
+
+    public Inquiry() {
+
+    }
+
+
+    
+
 }

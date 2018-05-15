cfe8912e7a24db3ecdfb908298bf6b2256e6cad8
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2018-05-15 09:20:26 +0200

    corrected abstract repo

--- src/main/java/Persistence/AbstractRepository.java
+++ src/main/java/Persistence/AbstractRepository.java
@@ -1,9 +1,5 @@
 package Persistence;
 
-import javax.sql.ConnectionPoolDataSource;
-import javax.sql.PooledConnection;
-//import javax.sql.PooledConnectionBuilder;
-
 import java.sql.*;
 import java.util.concurrent.*;
 
@@ -11,7 +7,6 @@ public abstract class AbstractRepository {
     private IConfiguration config;
     private static Executor executor;
     private static Connection conn;
-    private static PooledConnection conn2;
 
 
     public AbstractRepository() throws SQLException {
@@ -21,19 +16,19 @@ public abstract class AbstractRepository {
     private void initiate() throws SQLException {
         config=new Configuration();
         conn= DriverManager.getConnection(config.getServerUrl());
-        executor= Executors.newFixedThreadPool(4);
-        //conn2 = conn.createPooledConnectionBuilder();
+        executor= Executors.newFixedThreadPool(2);
     }
 
     protected ResponseMessage executeStm(final String statement){
         final ResponseMessage res = new ResponseMessage();
-        executor.execute(new Runnable() {
-            public void run() {
+        executor.execute(()-> {
+            {
                 try {
-                    PreparedStatement st = conn.prepareStatement(statement);
-                    res.setData(st.executeQuery());
-                    res.setResponseCode(ResponseCode.SUCCESS);
-
+                    synchronized (conn) {
+                        PreparedStatement st = conn.prepareStatement(statement);
+                        res.setData(st.executeQuery());
+                        res.setResponseCode(ResponseCode.SUCCESS);
+                    }
                 } catch (SQLException e) {
                     res.setData(null);
                     res.setResponseCode(ResponseCode.REJECTED);

f37e7c82a6c1a7e9c0594d9ba5d2c4a573fbeb95
Oskar <skakkristiansen@gmail.com>
2018-05-18 21:04:33 +0200

    Commented a little explanation

--- src/main/java/Domain/Validate.java
+++ src/main/java/Domain/Validate.java
@@ -16,16 +16,20 @@ public class Validate {
     }
 
     public boolean validateEmail(String email){
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
-                "[a-zA-Z0-9_+&*-]+)*@" +
+        //The emailRegex is a regular expression provided in a validation regex repository for email
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+   //^matches the beginning of the line, with the allowed specified characters
+                "[a-zA-Z0-9_+&*-]+)*@" +    //Matches 0 or more of the expression to the left, always including @
                 "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-                "A-Z]{1,7}$";
+                "A-Z]{1,7}$";  //$Matches the end of the line, with 1-7 allowed lenght
 
 
-        Pattern pat = Pattern.compile(emailRegex);
+        Pattern pat = Pattern.compile(emailRegex); //A compiled representation of a regular expression
+                                                    //must be compiled before made into a matcher object
+                                                    //Has no public constructor, initiated with the static compile method
         if (email == null)
             return false;
-        if(pat.matcher(email).matches())
+        //The matcher class interprets the pattern, also has no public constructor
+        if(pat.matcher(email).matches()) //matches the compiled regular expression against the character sequence received
             return true;
         else
             return false;
